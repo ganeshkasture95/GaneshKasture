c71e7229d5887c419e2edc768c516a8097f64509
Ganesh Kasture <ganeshkasture9595@gmail.com>
2023-10-23 20:14:02 +0530

    04
    04

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,27 +5,26 @@ import Avtr2 from '../../assets/R2.jpg'
 import Avtr3 from '../../assets/R3.jpg'
 import './testimonials.css'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-// Import Swiper styles
-import { Pagination } from 'swiper'
-import 'swiper/css'
-import 'swiper/css/pagination'
 
 const data = [
   {
     avatar : Avtr1,
     name : 'Anna Disuza',
-    review : 'Lorem ipsum dolorg elit. Necessitatibus delectus quam animi impedit. Repellat ratione quidem, pariatur temporibus porro earum.'
+    review : 'The software solution that Ganesh Kasture developed for my business has greatly improved our efficiency and productivity. It has streamlined several processes and has provided us with valuable insights and analytics. I am confident that this software solution will continue to have a positive impact on our business for years to come..'
   },
   {
     avatar : Avtr2,
     name : 'David Max',
-    review : 'Lorem ipsum dolor, ing elit. Necessitatibus delectus quam animi impedit. Repellat ratione quidem, pariatur temporibus porro earum.'
+    review : 'I highly recommend for app development, web development, and game development. Their team is skilled in creating custom applications, designing functional websites, and developing immersive games.'
   },
   {
     avatar : Avtr3,
     name : 'Sudhanshu',
+    review : 'Ganesh excels in content creation. Their team of content creators can help craft engaging written content, captivating videos, and eye-catching graphics that showcase your brand effectively.'
+  },
+  {
+    avatar : Avtr1,
+    name : 'Rinku',
     review : 'Lorem ipsum doloipisicing elit. Necessitatibus delectus quam animi impedit. Repellat ratione quidem, pariatur temporibus porro earum.'
   },
 ]
@@ -35,29 +34,24 @@ const Testimonials = () => {
     <section id='testimonials'>
       <h5>Reaview from clients</h5>
       <h2>Testimonials</h2>
-      <Swiper className="container textimonials__container" 
-       // install Swiper modules
-       modules={[Pagination]}
-       spaceBetween={40}
-       slidesPerView={1}
-       pagination={{ clickable: true }}
+      <div className="container textimonials__container" 
        >
         {
           data.map(({avatar,name,review},index)=>{
             return(
-              <SwiperSlide key ={index} className="textimonial">
+              <article key ={index} className="textimonial">
               <div className="client__avatar">
-                <img src={avatar}/>
+                <img src={avatar} article="gk"/>
               </div>
               <h5 className='client__name'>{name}</h5>
               <small className="client__review">
                 {review}
               </small>
-              </SwiperSlide>
+              </article>
             )
           })
         }
-      </Swiper>
+      </div>
     </section>
   )
 }
